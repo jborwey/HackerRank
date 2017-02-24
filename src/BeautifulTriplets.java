2ed29dfd6c0409b330ae9fca6faa04e4de6e24b0
Joshua Borwey <joshua.borwey@gmail.com>
2017-02-24 13:00:46 -0600

    HackerRank solutions to https://www.hackerrank.com/challenges/beautiful-triplets

--- src/BeautifulTriplets.java
+++ src/BeautifulTriplets.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 /**
  * Created by jb039k on 1/24/2017.
  */
 public class BeautifulTriplets {
+
+    public static int findBeauty(int beautiful, List<Integer> sequence){
+        int beautyCount = 0;
+        for(Integer integer : sequence){
+                if(sequence.contains(integer+beautiful) && sequence.contains((integer + 2*beautiful))){
+                    beautyCount = beautyCount + 1;
+                }
+            }
+
+        return beautyCount;
+    }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int length = sc.nextInt();
         int beautiful = sc.nextInt();
-        int[] sequence = new int[length];
+        List<Integer> sequence = new ArrayList<>();
 
         for(int i=0; i<length; i++){
-            sequence[i] = sc.nextInt();
+            sequence.add(sc.nextInt());
         }
+
+        int numTriplets = findBeauty(beautiful, sequence);
+        System.out.println(numTriplets);
     }
 }
